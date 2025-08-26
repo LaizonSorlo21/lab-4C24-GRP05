bf5074845de86ba4b9a9a8b6b0bc0d6a0f3833de
LaizonSorlo21 <kevin.flores.t@tecsup.edu.pe>
2025-08-25 22:36:41 -0500

    Add StudentReport.java

---
+++ src/StudentReport.java
@@ -0,0 +1,7 @@
+public class StudentReport {
+    public void generateReport(StudentModel student) {
+        System.out.println("Reporte del Estudiante");
+        System.out.println("Nombre: " + student.getName());
+        System.out.println("Edad: " + student.getAge());
+    }
+}
