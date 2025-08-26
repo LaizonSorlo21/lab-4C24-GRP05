62ee474b11f486a6fbb0d8cbec6925de117e5d4b
LaizonSorlo21 <kevin.flores.t@tecsup.edu.pe>
2025-08-25 22:37:06 -0500

    Add StudentView.java

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,5 +1,13 @@
 public class StudentController {
-    public void controller() {
-        // TO DO
+    public static void main(String[] args) {
+        StudentModel student = new StudentModel("Kevin Flores", 18);
+        StudentView view = new StudentView();
+        StudentReport report = new StudentReport();
+
+        // Mostrar información en pantalla
+        view.showStudent(student);
+
+        // Generar un reporte
+        report.generateReport(student);
     }
 }
---
+++ src/StudentView.java
@@ -0,0 +1,5 @@
+public class StudentView {
+    public void showStudent(StudentModel student) {
+        System.out.println("Estudiante: " + student.getName() + " | Edad: " + student.getAge());
+    }
+}
